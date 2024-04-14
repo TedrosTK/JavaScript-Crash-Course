edd438ea453c37ab82f9cf0b75b21d744862a6da
TedrosTK <tedydada@gmail.com>
2024-04-14 14:56:10 -0400

    Frontend SImplified For loop practice

--- app.js
+++ app.js
@@ -5,16 +5,22 @@
 // isStoreOpen && cash >= price ? console.log('give receipt') : console.log('do not give receipt')
 
 
-for (let i=1; i<=20; i++) {
-    let a = i % 3 === 0;
-    let b = i % 5 === 0;
-    if(a && b){
-        console.log(`${i} -> Frontend Simplified`);
-    }else if(a){
-        console.log(`${i} -> Frontend`);
-    }else if(b){
-        console.log(`${i} -> Simplified`);
-    }else {
-        console.log(`${i} -> ${i}`);
-    }
+// for (let i=1; i<=20; i++) {
+//     let a = i % 3 === 0;
+//     let b = i % 5 === 0;
+//     if(a && b){
+//         console.log(`${i} -> Frontend Simplified`);
+//     }else if(a){
+//         console.log(`${i} -> Frontend`);
+//     }else if(b){
+//         console.log(`${i} -> Simplified`);
+//     }else {
+//         console.log(`${i} -> ${i}`);
+//     }
+// }
+
+let str = "Frontend Simplified"
+for (let i = 0; i < str.length; i++){
+    console.log(str[i]);
 }
+
